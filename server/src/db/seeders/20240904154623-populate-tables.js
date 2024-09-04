58807731eec029cdc786a1512d5c5dc16fe1a814
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-04 18:16:28 +0200

    Server - Create seeder to populate the tables

---
+++ server/src/db/seeders/20240904154623-populate-tables.js
@@ -0,0 +1,97 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+     */
+
+    await queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          id: "069efd8f-4be8-476a-a3d3-27fc85a51c46",
+          firstName: "John",
+          lastName: "Doe",
+          email: "john.doe@example.com",
+          password: "password",
+          icon: "https://example.com/icon.png",
+          roleId: 1,
+          isAdmin: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          id: "784bce71-d775-45cf-ab8f-9a0144b80885",
+          firstName: "Jane",
+          lastName: "Doe",
+          email: "jane.doe@example.com",
+          password: "password",
+          icon: "https://example.com/icon.png",
+          roleId: 1,
+          isAdmin: false,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+
+    await queryInterface.bulkInsert(
+      "Clients",
+      [
+        {
+          id: "353f3f22-76df-4bf2-b3de-97f05ed30c3a",
+          name: "Google",
+          email: "google@gmail.com",
+          phone: "0123456789",
+          description: "Google is a multinational technology company.",
+          creatorId: "069efd8f-4be8-476a-a3d3-27fc85a51c46",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+
+    await queryInterface.bulkInsert(
+      "Projects",
+      [
+        {
+          id: "f2e70d4d-13a9-4600-84c9-e11503002ec3",
+          name: "Test Project",
+          statusId: 2,
+          budget: 10000.78,
+          description: "This is a test project.",
+          isInternalProject: false,
+          managerId: "784bce71-d775-45cf-ab8f-9a0144b80885",
+          clientId: "353f3f22-76df-4bf2-b3de-97f05ed30c3a",
+          creatorId: "069efd8f-4be8-476a-a3d3-27fc85a51c46",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+
+    await queryInterface.bulkDelete("Projects", null, {});
+    await queryInterface.bulkDelete("Clients", null, {});
+    await queryInterface.bulkDelete("Users", null, {});
+  },
+};
