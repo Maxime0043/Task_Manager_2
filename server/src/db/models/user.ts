f2c6b33813c8d961593ae1b647baa4829a658b7a
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-04 14:24:10 +0200

    Server - FIX - Wrong option in the Users table

--- server/src/db/models/user.ts
+++ server/src/db/models/user.ts
@@ -19,7 +19,7 @@ class User extends Model {
     type: DataType.UUID,
     defaultValue: DataType.UUIDV4,
     primaryKey: true,
-    autoIncrement: true,
+    allowNull: false,
   })
   id!: string;
 
