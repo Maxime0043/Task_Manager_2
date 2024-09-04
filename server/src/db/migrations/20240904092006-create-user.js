7b810c17bd665b2909222f0c5ccbfe5380fde7a6
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-04 11:59:44 +0200

    Server - Create migration for the table Users

---
+++ server/src/db/migrations/20240904092006-create-user.js
@@ -0,0 +1,63 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Users", {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        defaultValue: Sequelize.UUIDV4,
+        type: Sequelize.UUID,
+      },
+      firstName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      lastName: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      email: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      icon: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
+      role: {
+        allowNull: true,
+        references: {
+          model: "UserRoles",
+          key: "id",
+        },
+        type: Sequelize.INTEGER,
+      },
+      isAdmin: {
+        allowNull: false,
+        defaultValue: false,
+        type: Sequelize.BOOLEAN,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Users");
+  },
+};
