412730ea5e882cb9391442c1032bc4fab96c7bbf
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-05 10:06:49 +0200

    Server - Create table Tasks without associations

---
+++ server/src/db/models/task.ts
@@ -0,0 +1,76 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  ForeignKey,
+} from "sequelize-typescript";
+
+import User from "./user";
+import Project from "./project";
+import TaskStatus from "./task_status";
+
+export const TASK_PRIORITIES = {
+  HIGH: "high",
+  NORMAL: "normal",
+  LOW: "low",
+};
+
+@Table({
+  modelName: "Task",
+  paranoid: true,
+})
+class Task extends Model {
+  @Column({
+    type: DataType.UUID,
+    defaultValue: DataType.UUIDV4,
+    allowNull: false,
+    primaryKey: true,
+  })
+  id!: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  name!: string;
+
+  @Column({ type: DataType.DECIMAL(10, 2), allowNull: false })
+  timeEstimate!: number;
+
+  @Column({ type: DataType.DATE, allowNull: true })
+  deadline!: Date;
+
+  @Column({ type: DataType.INTEGER, allowNull: false, defaultValue: 0 })
+  percentDone!: number;
+
+  @ForeignKey(() => TaskStatus)
+  @Column({ type: DataType.INTEGER, allowNull: false })
+  statusId!: number;
+
+  @Column({ type: DataType.TEXT, allowNull: true })
+  description!: string;
+
+  @Column({
+    type: DataType.ENUM(...Object.values(TASK_PRIORITIES)),
+    allowNull: false,
+    defaultValue: TASK_PRIORITIES.NORMAL,
+  })
+  priority!: string;
+
+  @Column({ type: DataType.INTEGER, allowNull: false, defaultValue: 0 })
+  position!: number;
+
+  @ForeignKey(() => Project)
+  @Column({ type: DataType.UUID, allowNull: false })
+  projectId!: string;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.UUID, allowNull: false })
+  creatorId!: string;
+
+  /**
+   * ASSOCIATIONS
+   */
+
+  // Add associations here
+}
+
+export default Task;
