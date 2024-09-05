ae86f36ddfbeea19052a10fd38636624ee618572
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-05 09:00:38 +0200

    Server - Create migration for the table TaskStatus

---
+++ server/src/db/migrations/20240905064552-create-task-status.js
@@ -0,0 +1,45 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("TaskStatus", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+      label: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      color: {
+        allowNull: false,
+        validate: {
+          is: /^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/,
+        },
+        type: Sequelize.STRING,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      deletedAt: {
+        allowNull: true,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("TaskStatus");
+  },
+};
