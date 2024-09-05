15de4c4bc6897a499726b3a24405de0f16f9b01f
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-05 14:29:02 +0200

    Server - Create table TaskUsers without associations

---
+++ server/src/db/models/task_users.ts
@@ -0,0 +1,40 @@
+import {
+  Table,
+  Column,
+  Model,
+  DataType,
+  ForeignKey,
+} from "sequelize-typescript";
+
+import User from "./user";
+import Task from "./task";
+
+@Table({
+  modelName: "TaskUsers",
+  tableName: "TaskUsers",
+})
+class TaskUsers extends Model {
+  @Column({
+    type: DataType.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  })
+  id!: number;
+
+  @ForeignKey(() => Task)
+  @Column({ type: DataType.UUID, allowNull: false })
+  taskId!: string;
+
+  @ForeignKey(() => User)
+  @Column({ type: DataType.UUID, allowNull: false })
+  userId!: string;
+
+  /**
+   * ASSOCIATIONS
+   */
+
+  // Add associations here
+}
+
+export default TaskUsers;
