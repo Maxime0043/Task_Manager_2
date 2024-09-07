29b88d251bb42148e38d301fd370d96f966e0e43
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-07 11:17:46 +0200

    Server - Create the /auth/signin route

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import Joi from "joi";
+import bcrypt from "bcrypt";
 
 import User from "../db/models/user";
 
@@ -39,5 +40,40 @@ export async function signup(req: Request, res: Response) {
     return res.status(201).json(user);
   } catch (err) {
     return res.status(409).json({ error: err });
+  }
+}
+
+export async function signin(req: Request, res: Response) {
+  const payload = req.body;
+
+  // Create JOI Schema to validate the payload
+  const schema = Joi.object({
+    email: Joi.string().trim().email().max(255).required(),
+    password: Joi.string().trim().min(6).max(16).required(),
+  });
+
+  // Validate the payload
+  const { value, error } = schema.validate(payload, { abortEarly: false });
+
+  if (error) {
+    return res
+      .status(400)
+      .json({ error: error.details.map((error) => error.message) });
+  }
+
+  // Find the user by email
+  const user = await User.findOne({ where: { email: value.email } });
+
+  if (!user) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  // Check if the password is correct
+  const isPasswordValid = await bcrypt.compare(value.password, user.password);
+
+  if (!isPasswordValid) {
+    return res.status(400).json({ error: "Invalid password" });
+  } else {
+    return res.status(200).json(user);
   }
 }
--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -3,10 +3,11 @@ import express from "express";
 const router = express.Router();
 
 // Import Controllers
-import { signup } from "../controllers/auth.controller";
+import { signin, signup } from "../controllers/auth.controller";
 
 // Define the API routes
 router.get("/signup", signup);
+router.get("/signin", signin);
 
 // Export the router
 export default router;
