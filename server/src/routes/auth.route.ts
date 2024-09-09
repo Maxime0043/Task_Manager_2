10a60e5ecbc6de9b5898978a3b8434dfa09b9eac
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-09 16:49:34 +0200

    Server - Apply the auth middlewares

--- server/src/routes/auth.route.ts
+++ server/src/routes/auth.route.ts
@@ -2,13 +2,16 @@ import express from "express";
 
 const router = express.Router();
 
+// Import Middlewares
+import { authRequired } from "../middlewares/auth.middleware";
+
 // Import Controllers
 import { signin, signup, signout } from "../controllers/auth.controller";
 
 // Define the API routes
 router.post("/signup", signup);
 router.post("/signin", signin);
-router.post("/signout", signout);
+router.post("/signout", [authRequired], signout);
 
 // Export the router
 export default router;
