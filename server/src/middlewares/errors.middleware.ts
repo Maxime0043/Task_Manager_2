8b6177ebb685e39ad85cbc31556cbc81c7b68c26
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-09 10:15:57 +0200

    Server - Use the JoiError in the express error middleware

--- server/src/middlewares/errors.middleware.ts
+++ server/src/middlewares/errors.middleware.ts
@@ -1,4 +1,5 @@
 import { NextFunction, Request, Response } from "express";
+import JoiError from "../errors/JoiError";
 
 export function errorHandler(
   err: Error,
@@ -6,6 +7,10 @@ export function errorHandler(
   res: Response,
   next: NextFunction
 ) {
+  if (err instanceof JoiError) {
+    return res.status(err.statusCode).json({ error: err.errors });
+  }
+
   console.error(err);
-  res.status(500).json({ error: "Internal Server Error" });
+  return res.status(500).json({ error: "Internal Server Error" });
 }
