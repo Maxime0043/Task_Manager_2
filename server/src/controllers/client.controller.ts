66f72b73117f94f6bb28932e0666b999b2bf0f9f
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-10 09:12:50 +0200

    Server - Create the the /clients/:id route

--- server/src/controllers/client.controller.ts
+++ server/src/controllers/client.controller.ts
@@ -1,8 +1,25 @@
 import { Request, Response } from "express";
 import Client from "../db/models/client";
+import SimpleError from "../errors/SimpleError";
 
 export async function listAll(req: Request, res: Response) {
   const clients = await Client.findAll();
 
   return res.status(200).json({ clients });
+}
+
+export async function details(req: Request, res: Response) {
+  const { id } = req.params;
+
+  const client = await Client.findByPk(id);
+
+  if (!client) {
+    throw new SimpleError({
+      statusCode: 404,
+      name: "not_found",
+      message: "Client not found",
+    });
+  }
+
+  return res.status(200).json({ client });
 }
--- server/src/routes/client.route.ts
+++ server/src/routes/client.route.ts
@@ -5,10 +5,11 @@ const router = express.Router();
 // Import Middlewares
 
 // Import Controllers
-import { listAll } from "../controllers/client.controller";
+import { details, listAll } from "../controllers/client.controller";
 
 // Define the API routes
 router.get("/", listAll);
+router.get("/:id", details);
 
 // Export the router
 export default router;
