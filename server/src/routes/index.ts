e75e6319baa21c567e033e5227a32b2b889aa84f
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-11 09:50:13 +0200

    Server - Initialize the /tasks route

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -9,11 +9,13 @@ import { auth, authRequired } from "../middlewares/auth.middleware";
 import authRoutes from "./auth.route";
 import clientRoutes from "./client.route";
 import projectRoutes from "./project.route";
+import taskRoutes from "./task.route";
 
 // Define the API routes
 router.use("/auth", [auth], authRoutes);
 router.use("/clients", [auth, authRequired], clientRoutes);
 router.use("/projects", [auth, authRequired], projectRoutes);
+router.use("/tasks", [auth, authRequired], taskRoutes);
 
 // Export the router
 export default router;
