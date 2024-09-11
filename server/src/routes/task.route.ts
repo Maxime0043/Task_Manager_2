f59143b4b67b1f401050b98bc710d2979b8afe6a
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-11 14:42:43 +0200

    Server - Initialize the /tasks/scheduled route

--- server/src/routes/task.route.ts
+++ server/src/routes/task.route.ts
@@ -14,6 +14,9 @@ import {
   update,
 } from "../controllers/task.controller";
 
+// Import routes
+import taskScheduledRoutes from "./task_scheduled.route";
+
 // Define the API routes
 router.get("/", listAll);
 router.get("/:id", details);
@@ -21,6 +24,9 @@ router.post("/", create);
 router.put("/:id", update);
 router.delete("/:id", remove);
 router.post("/restore/:id", restore);
+
+// Define the API routes
+router.use("/scheduled", taskScheduledRoutes);
 
 // Export the router
 export default router;
---
+++ server/src/routes/task_scheduled.route.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const router = express.Router();
+
+// Import Middlewares
+
+// Import Controllers
+
+// Define the API routes
+
+// Export the router
+export default router;
