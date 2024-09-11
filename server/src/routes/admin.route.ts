0783b984cc12e665c9fdee6ca879078e3711e544
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-11 22:20:21 +0200

    Server - Initialize the /admin route

---
+++ server/src/routes/admin.route.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+// Import Middlewares
+
+// Import Controllers
+
+// Import routes
+
+// Define the API routes
+
+// Export the router
+export default router;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -10,12 +10,15 @@ import authRoutes from "./auth.route";
 import clientRoutes from "./client.route";
 import projectRoutes from "./project.route";
 import taskRoutes from "./task.route";
+import adminRoutes from "./admin.route";
 
 // Define the API routes
 router.use("/auth", [auth], authRoutes);
 router.use("/clients", [auth, authRequired], clientRoutes);
 router.use("/projects", [auth, authRequired], projectRoutes);
 router.use("/tasks", [auth, authRequired], taskRoutes);
+
+router.use("/admin", [auth, authRequired], adminRoutes);
 
 // Export the router
 export default router;
