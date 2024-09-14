53bc2d78f562ee9db646076711dfd29d5458a2bf
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-14 11:00:06 +0200

    Server - Create the PUT /admin/users/:id route

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -49,3 +49,69 @@ export async function create(req: Request, res: Response) {
     throw err;
   }
 }
+
+export async function updateOther(req: Request, res: Response) {
+  const { id } = req.params;
+
+  // Validate the params
+  const errorParams = verifyIdIsUUID(req.params);
+
+  if (errorParams) {
+    throw new JoiError({ error: errorParams, isUrlParam: true });
+  }
+
+  // Find the user to update
+  const user = await User.findByPk(id);
+
+  if (!user) {
+    throw new SimpleError({
+      statusCode: 404,
+      name: "not_found",
+      message: "User not found",
+    });
+  }
+
+  const payload = req.body;
+
+  // Create JOI Schema to validate the payload
+  const schema = Joi.object({
+    firstName: Joi.string().trim().max(255),
+    lastName: Joi.string().trim().max(255),
+    email: Joi.string().trim().email().max(255),
+    password: Joi.string().trim().min(6).max(16),
+    passwordConfirmation: Joi.string()
+      .trim()
+      .min(6)
+      .max(16)
+      .valid(Joi.ref("password"))
+      .when("password", {
+        is: Joi.exist(),
+        then: Joi.required(),
+        otherwise: Joi.forbidden(),
+      }),
+    icon: Joi.string().trim().uri().max(255),
+    roleId: Joi.number().integer().min(1),
+    isAdmin: Joi.boolean(),
+  });
+
+  // Validate the payload
+  const { value, error } = schema.validate(payload, { abortEarly: false });
+
+  if (error) {
+    throw new JoiError({ error });
+  }
+
+  // Continue with the user update process
+  try {
+    // Update the user
+    const updatedUser = await user.update(value);
+
+    return res.status(200).json({ user: updatedUser });
+  } catch (err) {
+    if (err instanceof BaseError) {
+      throw new SequelizeError({ statusCode: 409, error: err });
+    }
+
+    throw err;
+  }
+}
--- server/src/routes/admin.route.ts
+++ server/src/routes/admin.route.ts
@@ -5,12 +5,16 @@ const router = express.Router();
 // Import Middlewares
 
 // Import Controllers
-import { create as createUser } from "../controllers/user.controller";
+import {
+  create as createUser,
+  updateOther as updateUser,
+} from "../controllers/user.controller";
 
 // Import routes
 
 // Define the API routes
 router.post("/users", createUser);
+router.put("/users/:id", updateUser);
 
 // Export the router
 export default router;
