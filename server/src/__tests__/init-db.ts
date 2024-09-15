43a70c8fec0c199c71e44f8848ecd2ce848d2ab2
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-15 10:15:34 +0200

    Server - TEST - Add function to populate the UserRoles table

--- server/src/__tests__/init-db.ts
+++ server/src/__tests__/init-db.ts
@@ -121,6 +121,22 @@ export default async function initDB() {
   ]);
 }
 
+export async function populateUserRoles() {
+  // Create the userRoles
+  const userRoles: any = [];
+
+  for (let i = 1; i <= 20; i++) {
+    userRoles.push({
+      name: `role ${i}`,
+      label: `Role ${i}`,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+  }
+
+  await UserRoles.bulkCreate(userRoles);
+}
+
 export async function populateClients(creatorId: string) {
   // Create the clients
   const clients: any = [];
