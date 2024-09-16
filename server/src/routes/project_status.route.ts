17e732a1ef70837c85109268004d96da683dfdc3
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-16 11:26:35 +0200

    Server - Initialize the /projects/status route

---
+++ server/src/routes/project_status.route.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const router = express.Router();
+
+// Import Middlewares
+
+// Import Routes
+
+// Import Controllers
+
+// Define the API routes
+
+// Export the router
+export default router;
