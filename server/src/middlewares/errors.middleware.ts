585962780c800ebd4211aaca32eede8523413531
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-16 18:04:52 +0200

    Server - Use the MulterError in the express error middleware

--- server/src/middlewares/errors.middleware.ts
+++ server/src/middlewares/errors.middleware.ts
@@ -1,8 +1,10 @@
 import { NextFunction, Request, Response } from "express";
+import multer from "multer";
 
 import JoiError from "../errors/JoiError";
 import SequelizeError from "../errors/SequelizeError";
 import SimpleError from "../errors/SimpleError";
+import MulterError from "../errors/MulterError";
 
 export function errorHandler(
   err: Error,
@@ -12,8 +14,11 @@ export function errorHandler(
 ) {
   if (err instanceof JoiError || err instanceof SequelizeError) {
     return res.status(err.statusCode).json({ errors: err.errors });
-  } else if (err instanceof SimpleError) {
+  } else if (err instanceof SimpleError || err instanceof MulterError) {
     return res.status(err.statusCode).json({ error: err.error });
+  } else if (err instanceof multer.MulterError) {
+    const multerErr = new MulterError({ error: err });
+    return res.status(multerErr.statusCode).json({ error: multerErr.error });
   }
 
   console.error(err);
