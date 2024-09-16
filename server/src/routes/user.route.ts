933cebea31a8912b4f08c318275c46a790604397
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-16 10:42:34 +0200

    Server - Create the PUT /users/info route

--- server/src/routes/user.route.ts
+++ server/src/routes/user.route.ts
@@ -5,11 +5,12 @@ const router = express.Router();
 // Import Middlewares
 
 // Import Controllers
-import { info, listAll } from "../controllers/user.controller";
+import { info, listAll, update } from "../controllers/user.controller";
 
 // Define the API routes
 router.get("/", listAll);
 router.get("/info", info);
+router.put("/info", update);
 
 // Export the router
 export default router;
