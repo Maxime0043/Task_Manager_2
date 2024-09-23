9c9ed020aeae83f3ea1f200da956b8e6ae0716e6
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-23 12:25:49 +0000

    Server - WS - Create the conversation middleware

---
+++ server/src/websocket/middlewares/conversation.middleware.ts
@@ -0,0 +1,63 @@
+import { Socket } from "socket.io";
+import SocketError from "../errors/SocketError";
+import Conversation from "../../db/models/conversation";
+import ConversationUsers from "../../db/models/conversation_users";
+
+export default function (socket: Socket) {
+  socket.use(async ([event, ...args], next) => {
+    if (event !== "join-conversation") {
+      return next();
+    }
+
+    // Validate the data
+    const data = args[0];
+
+    if (!data || !data.id) {
+      return next(
+        new SocketError({
+          event,
+          name: "invalid-data",
+          message: "Invalid data",
+        })
+      );
+    }
+
+    // Verify if the conversation exists
+    const conversation = await Conversation.findByPk(data.id, {
+      include: [ConversationUsers],
+    });
+
+    if (!conversation) {
+      socket.data.conversationId = null;
+
+      return next(
+        new SocketError({
+          event,
+          name: "conversation-not-found",
+          message: "Conversation not found",
+        })
+      );
+    }
+
+    // Verify if the user is part of the conversation
+    if (
+      conversation.conversationUsers &&
+      conversation.conversationUsers.aUserId !== socket.data.user.id &&
+      conversation.conversationUsers.bUserId !== socket.data.user.id
+    ) {
+      socket.data.conversationId = null;
+
+      return next(
+        new SocketError({
+          event,
+          name: "user-not-in-conversation",
+          message: "User not in conversation",
+        })
+      );
+    }
+
+    // Define the conversation id
+    socket.data.conversationId = conversation.id;
+    next();
+  });
+}
