328f187e84afa32025e64622a516c5f4a16401e9
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-23 13:01:21 +0000

    Server - FIX - WS - Forget to define the Server to CLient events of the conversation

--- server/src/websocket/utils/interfaces.ts
+++ server/src/websocket/utils/interfaces.ts
@@ -1,6 +1,10 @@
-export interface ClientToServerEvents {}
+export interface ClientToServerEvents {
+  "join-conversation": (data: Record<string, string>) => void;
+}
 export interface ServerToClientEvents {
   error: (data: object) => void;
+  "join-conversation": (data: Record<string, string>) => void;
+  "leave-conversation": () => void;
 }
 export interface InterServerEvents {}
 export interface SocketData {
