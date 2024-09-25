8e9e902ce7729a5a10cdf7561f62a4faaa27aa94
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-25 08:43:31 +0000

    Client - Implement the signin form in the Signin page

--- client/src/pages/Signin.tsx
+++ client/src/pages/Signin.tsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 function Signin() {
+  const [showError, setShowError] = useState(false);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setShowError(false);
+
+    const form = event.currentTarget;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
+
+    fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/signin`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => {
+        if (response.status !== 200) {
+          setShowError(true);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        setShowError(true);
+      });
+  }
+
   return (
     <div>
-      <h1>Signin</h1>
+      <h1>Se Connecter</h1>
+
+      {showError && <p>Email ou mot de passe incorrect!</p>}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label>
+
+        <label>
+          Mot de passe:
+          <input type="password" name="password" />
+        </label>
+
+        <button type="submit">Connexion</button>
+      </form>
     </div>
   );
 }
