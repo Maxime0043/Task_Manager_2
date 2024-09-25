014d0c3278e829020feb9f6ab4a118f83428666a
Maxime PINNA <57800452+Maxime0043@users.noreply.github.com>
2024-09-25 13:39:56 +0000

    Client - Create the AuthSlice for redux

---
+++ client/src/app/actions/authActions.ts
@@ -0,0 +1,15 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+
+export const signinUser = createAsyncThunk(
+  "auth/signin",
+  (data: { email: string; password: string }) => {
+    return fetch(`${import.meta.env.VITE_API_URL}/api/v1/auth/signin`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+  }
+);
---
+++ client/src/app/features/auth.slice.ts
@@ -0,0 +1,62 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { signinUser } from "../actions/authActions";
+import Cookies from "js-cookie";
+
+export type InitialStateType = {
+  loading: boolean;
+  error: null | string;
+  success: boolean;
+  userSid: null | string;
+  user: {
+    lastName: null | string;
+    firstName: null | string;
+    icon: null | string;
+  };
+};
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    loading: false,
+    error: null,
+    success: false, // This is used to display a success message to the user
+    userSid: null,
+    user: {
+      lastName: null,
+      firstName: null,
+      icon: null,
+    },
+  } as InitialStateType,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      // Signin
+      .addCase(signinUser.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+        state.success = false;
+        state.userSid = null;
+        state.user = {
+          lastName: null,
+          firstName: null,
+          icon: null,
+        };
+      })
+      .addCase(signinUser.fulfilled, (state, action) => {
+        if (action.payload.status !== 200) {
+          state.loading = false;
+          state.error = action.payload.statusText;
+        } else {
+          state.loading = false;
+          state.success = true;
+          state.userSid = Cookies.get("connect.sid") ?? null;
+        }
+      })
+      .addCase(signinUser.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.error.message as string;
+      });
+  },
+});
+
+export default authSlice.reducer;
